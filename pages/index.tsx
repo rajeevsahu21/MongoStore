8bdaa8949297c3dc24504de9db22511904f0e60f
Rajeev Sahu <rajeevsahu210@gmail.com>
2023-02-24 15:45:14 +0530

    Update name in Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Mongo Store</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="bg-white w-full min-h-screen">
