58ab216fa97350990ba70ed88955b3d6c1886119
Rajeev Sahu <rajeevsahu210@gmail.com>
2023-02-24 14:33:42 +0530

    finished initial App

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,33 +1,33 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import { useState, useEffect } from "react";
 import * as Realm from "realm-web";
+import Category from "../../components/Category";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Pagination from "../../components/Pagination";
+import Products from "../../components/Products";
 
-import Category from "../components/Category";
-import Container from "../components/Container";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Pagination from "../components/Pagination";
-import Products from "../components/Products";
-
-const Home: NextPage = () => {
+export default function Home() {
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
     const getProducts = async () => {
-      const REALM_APP_ID = "products-rbwso";
+      // add your Realm App Id to the .env.local file
+      const REALM_APP_ID = process.env.NEXT_PUBLIC_REALM_APP_ID;
       const app = new Realm.App({ id: REALM_APP_ID });
       const credentials = Realm.Credentials.anonymous();
       try {
         const user = await app.logIn(credentials);
         const allProducts = await user.functions.getAllProducts();
-        setProducts(allProducts);
-      } catch (err) {
-        console.error("Failed to log in", err);
+        setProducts(() => allProducts);
+      } catch (error) {
+        console.error(error);
       }
     };
     getProducts();
   }, []);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -37,9 +37,8 @@ const Home: NextPage = () => {
       <div className="bg-white w-full min-h-screen">
         <Header />
         <Container>
-          <Hero />
           <Category
-            category="Tech Wear"
+            category="All Products"
             categoryCount={`${products.length} Products`}
           />
           <Products products={products} />
@@ -49,6 +48,4 @@ const Home: NextPage = () => {
       </div>
     </div>
   );
-};
-
-export default Home;
+}
